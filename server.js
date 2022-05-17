72d65067daedfac375d57ab6bfe127a6776fc4e0
Michael Kerr <bulluffalo@gmail.com>
2022-05-16 21:32:19 -0700

    Implement Sequelize CLI

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.PG_URI)
-
-try {
-    sequelize.authenticate()
-    console.log(`Connected with Sequelize at port 5432`)
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`)
-}
-
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
