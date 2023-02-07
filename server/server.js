f8d159cbe552132ff9bfa67dd82f9410a16594ac
Michael Kerr <bulluffalo@gmail.com>
2023-02-07 00:46:25 -0700

    add cors

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
 const path = require("path");
+const cors = require('cors');
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "..", "build")));
